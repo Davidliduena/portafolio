7e908374147cf4aeb233ddd6221fd15dca0e71e3
Davidliduena <Davidliduena13@gmail.com>
2022-07-13 18:09:18 -0500

    cambios

--- index.php
+++ index.php
@@ -16,39 +16,13 @@
 
 <body id="inicio">
     <header class="container">
-        <nav class="navbar navbar-expand-md mb-4">
-            <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarCollapse">
-                    <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="index.html">Inicio</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="sobre-mi.html">Sobre mí</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="proyectos.html">Proyectos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link " href="contacto.html">Contacto</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="d-inline">
-                    <a href="" class="btn btn-rojo">Descargar mi CV <i class="fas fa-download"></i></a>
-                </div>
-            </div>
-        </nav>
+    <?php include_once "menu.php";?>
     </header>
     <main class="container">
         <main class="container">
             <div class="row">
                 <div class="col-12 mt-4 text-center div-cohete">
-                    <a href="proyectos.html"><img src="imagenes/cohete.svg" class="cohete"></a>
+                    <a href="proyectos.php"><img src="imagenes/cohete.svg" class="cohete"></a>
                 </div>
             </div>
             <div class="row">
--- sobre-mi.php
+++ sobre-mi.php
@@ -16,34 +16,7 @@
 
 <body id="sobre-mi">
     <header class="container">
-        <nav class="navbar navbar-expand-md mb-4">
-            <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarCollapse">
-                    <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                        <li class="nav-item">
-                            <a class="nav-link" href="index.html">Inicio</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" href="sobre-mi.html">Sobre mí</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="proyectos.html">Proyectos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link " href="contacto.html">Contacto</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="d-inline">
-                    <a href="" class="btn btn-rojo">Descargar mi CV <i class="fas fa-download"></i></a>
-                </div>
-            </div>
-            </div>
-        </nav>
+       <?php include_once "menu.php";?>
     </header>
     <main class="container">
         <div class="container">
@@ -52,7 +25,7 @@
                     <h1 class="mt-sm-4 pb-sm-4">Sobre mí</h1>
                     <p class="pb-sm-5">Apasionado por la tecnología y gestión de proyectos. Soy programador Full Stack y
                         Base de datos.</p>
-                    <a href="contacto.html" class="btn btn-rojo">Enviar mensaje</a>
+                    <a href="contacto.php" class="btn btn-rojo">Enviar mensaje</a>
 
                 </div>
                 <div class="col-sm-3 col-12 mt-4 offset-sm-2">
